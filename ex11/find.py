49c6157fe7ad3a1952c26bfb9d7eae5315d9550e
asalek <ayoub.salek8599@gmail.com>
2023-04-03 02:33:45 +0000

    add pokemon api user with python

---
+++ ex11/find.py
@@ -0,0 +1,23 @@
+import requests
+
+# Set the base URL for the PokeAPI
+BASE_URL = 'https://pokeapi.co/api/v2/'
+
+# Prompt the user for a Pokemon name
+pokemon_name = input('Enter a Pokemon name: ')
+
+# Send an HTTP GET request to the PokeAPI to retrieve information about the Pokemon
+# response = requests.get(f'{BASE_URL}pokemon/{pokemon_name}')
+response = requests.get(BASE_URL + 'pokemon/' + pokemon_name)
+
+# If the response is successful, retrieve the JSON data
+if response.status_code == 200:
+    pokemon_data = response.json()
+    # Extract the relevant information from the JSON data
+    name = pokemon_data['name']
+    abilities = [ability['ability']['name'] for ability in pokemon_data['abilities']]
+    # Print the information
+    print('%s abilities:' % name)
+    print(', '.join(abilities))
+else:
+    print('Error retrieving data')
